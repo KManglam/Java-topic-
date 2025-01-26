567b6dfda28e747e4bcf198186b0ae1e6c1f3206
Kumar Manglam <kmglm04718@gmail.com>
2025-01-26 15:51:04 +0530

    Add files via upload

---
+++ Prob2.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+
+public class Prob2 {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the first number: ");
+        int a = input.nextInt();
+        System.out.println("Enter the second number: ");
+        int b = input.nextInt();
+        int c = a + b;
+        System.out.println("Sum of two numbers is: " + c);
+    }
+}
