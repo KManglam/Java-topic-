12bf48e2271c87a39a9a2d2a398de1b2ae7c4a2f
Kumar Manglam <kmglm04718@gmail.com>
2025-01-26 15:51:48 +0530

    Add files via upload

---
+++ userinput.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+
+public class userinput {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        System.out.print("Who are you ? ");
+        String name = input.nextLine();
+        System.out.print("Hi " + name);
+        System.out.println(name + " Also tell me your age:");
+        int age = input.nextInt();
+        System.out.println("Your age is " + age );
+    }
+}
