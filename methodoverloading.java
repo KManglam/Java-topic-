5c732b5084d3a4b0508b92b6eb333eccc780aff4
Kumar Manglam <kmglm04718@gmail.com>
2025-01-26 15:55:56 +0530

    Add files via upload

---
+++ methodoverloading.java
@@ -0,0 +1,18 @@
+class methodoverloading {
+    int add(int a,int b){
+        return a+b;
+    }
+    int add(int a, int b, int c){
+        return a+b+c;
+    }
+    double add(double a,double b){
+        return a+b;
+    }
+
+    public static void main(String[] args) {
+        methodoverloading obj = new methodoverloading();
+        System.out.println("Add: "+ obj.add(5,3));
+        System.out.println("Add: "+ obj.add(5,1,3));
+        System.out.println("Add: "+ obj.add(7.5,7.5));
+    }
+}
