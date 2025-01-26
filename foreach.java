6507e1b72e6ceb9c7935e6378c3a625af4dcd858
Kumar Manglam <kmglm04718@gmail.com>
2025-01-26 15:54:28 +0530

    Add files via upload

---
+++ foreach.java
@@ -0,0 +1,8 @@
+public class foreach {
+    public static void main(String[] args) {
+        String X[]={"Abhi","Amit","Div","Aadi"};
+        for(String Y:X){
+            System.out.println(Y);
+        }
+    }
+}
---
+++ foreachloop.java
@@ -0,0 +1,10 @@
+public class foreachloop {
+    public static void main(String[] args) {
+        System.out.println("Vegetables are: ");
+        String a[]={"Aloo","Payaj","Tamatar","Bhindi"};
+        for(String b:a){
+            System.out.println(b);
+
+        }
+    }
+}
