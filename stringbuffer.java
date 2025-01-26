e5044241047849986a2fd3b72733fb241fa06160
Kumar Manglam <kmglm04718@gmail.com>
2025-01-26 15:57:09 +0530

    Add files via upload

---
+++ stringbuffer.java
@@ -0,0 +1,9 @@
+public class stringbuffer {
+    public static void main(String[]m){
+        StringBuffer a = new StringBuffer("Kumar");
+        a.append(" Manglam");   // appending a new string to the existing string
+        System.out.println("New String: "+ a);
+        // String Buffer Method  //
+        System.out.println(a.reverse());
+    }
+}
---
+++ stringbuilder.java
@@ -0,0 +1,7 @@
+public class stringbuilder {
+    public static void main(String[] args) {
+        StringBuilder b = new StringBuilder("Hello");
+        b.append(" World");   // appending a new string to the existing string
+        System.out.println("New String: "+ b);
+    }
+}
