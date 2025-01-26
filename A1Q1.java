1f9069aa0baefaaf25177edad3fd626e9e672968
Kumar Manglam <kmglm04718@gmail.com>
2025-01-26 15:48:04 +0530

    Add files via upload

---
+++ A1Q1.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+ public class A1Q1{
+    public static void main(String[] args) {
+        Scanner obj = new Scanner(System.in);
+        System.out.print("Enter the number: ");
+        int n;
+        n = obj.nextInt();
+        double sum = 0.0;
+        for (int i=1;i<=n;i++){
+            sum+=1.0/i;
+        }
+        System.out.println("The Sum of Harmonic Series: " + sum);
+
+
+    }
+}
