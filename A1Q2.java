77d2d2dbceee8e52151fdd16ffa2c38ce0f3e3af
Kumar Manglam <kmglm04718@gmail.com>
2025-01-26 15:48:39 +0530

    Add files via upload

---
+++ A1Q2.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class A1Q2 {
+    public static void main(String[] args) {
+        System.out.print("Enter the Price of an Item: ");
+        Scanner in=new Scanner(System.in);
+        float Price, Paise;
+        Price = in.nextFloat();
+        Paise = Price*100;
+        // type conversion
+        //int x = (int)Paise;
+        System.out.println("Price of an Item in Paise: " + (int)Paise);
+
+    }
+}
