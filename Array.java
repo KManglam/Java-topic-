8b72dc817c51213a3ee26e5f4384c28c63e1426b
Kumar Manglam <kmglm04718@gmail.com>
2025-01-26 15:53:06 +0530

    Add files via upload

---
+++ Array.java
@@ -0,0 +1,12 @@
+public class Array {
+    public static void main(String[] args) {
+        int a[]={1,2,1,4,5,8};
+        int i;
+        for(i=0;i<=5;i++){
+            System.out.println(a[i]);
+        }
+        for(int b:a){
+            System.out.println(b);
+        }
+    }
+}
