5bb64247c623dcd9f648253356851b07493bd359
Kumar Manglam <kmglm04718@gmail.com>
2025-01-31 15:45:13 +0530

    Update A1Q1.java

--- A1Q1.java
+++ A1Q1.java
@@ -1,5 +1,6 @@
 import java.util.Scanner;
- public class A1Q1{
+ 
+public class A1Q1{
     public static void main(String[] args) {
         Scanner obj = new Scanner(System.in);
         System.out.print("Enter the number: ");
