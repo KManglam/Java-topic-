8ec1d037c82959a85a27c4e68e835777a6ab19dd
Kumar Manglam <kmglm04718@gmail.com>
2025-02-05 17:35:24 +0530

    Update constload.java

--- constload.java
+++ constload.java
@@ -1,6 +1,7 @@
 
 public class constload {
 
+    
     constload(int a,int b){ System.out.println(a+b); }
     constload(int a, int b, int c){ System.out.println(a+b+c); }
     constload(double a,double b){ System.out.println("a+b"); }
