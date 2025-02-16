5abfc3e42ef194ceed22a323c99aaecb5427d059
Kumar Manglam <kmglm04718@gmail.com>
2025-02-16 14:18:13 +0530

    Update Calculate.java

--- Calculate.java
+++ Calculate.java
@@ -2,6 +2,7 @@ class Calculate{
     static int cube(int x){
         return x*x*x;
     }
+    
     public static void main(String[] args) {
         int result =Calculate.cube(4);
         System.out.println(result);
