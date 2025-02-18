3fac7536ba5f4315d025475570b58d51b5f98916
Kumar Manglam <kmglm04718@gmail.com>
2025-02-18 19:42:58 +0530

    Update Prob2.java

--- Prob2.java
+++ Prob2.java
@@ -1,5 +1,6 @@
 import java.util.Scanner;
 public class Prob2 {
+    
     public static void main(String[] args){
         Scanner input = new Scanner(System.in);
         System.out.println("Enter the first number: ");
