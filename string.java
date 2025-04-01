9fe0ea007c03d682e8522858395bc538fa92409f
Kumar Manglam <kmglm04718@gmail.com>
2025-04-01 18:43:22 +0530

    Update string.java

--- string.java
+++ string.java
@@ -2,7 +2,8 @@ public class string {
     public static void main(String[]M){
     String a="Zebra";
     String b=new String("Cat");
-      
+
+        
         System.out.println(a);
         System.out.println(b);
     }
