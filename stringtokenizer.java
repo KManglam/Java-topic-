ce4dc3fc6c17154c47a56898d5c164a6a9d63ec4
Kumar Manglam <kmglm04718@gmail.com>
2025-04-16 10:48:13 +0530

    Update stringtokenizer.java

--- stringtokenizer.java
+++ stringtokenizer.java
@@ -3,7 +3,7 @@ public class stringtokenizer {
     public static void main(String[] args) {
         StringTokenizer d = new StringTokenizer("My name is Km");
         while (d.hasMoreTokens()) {
-            System.out.println(d.nextToken());
+             System.out.println(d.nextToken());
         }
     }
 }
