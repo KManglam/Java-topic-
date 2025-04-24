41c79cb187d621f9df778c2139fbb6ca3ce229dc
Kumar Manglam <kmglm04718@gmail.com>
2025-04-24 15:03:58 +0530

    Create fruitprob.java

---
+++ fruit.java
@@ -0,0 +1,39 @@
+//  Problem Statement
+//  With a given array of characters where each character represents a fruit tree, place the maximum number of fruits in each of 2 baskets. The only restriction is that each basket can have only one type of fruit.
+//  You can start with any tree, but you can’t skip a tree once you have started. You will pick one fruit from each tree until you cannot, i.e., you will stop when you have to pick from a third fruit type.
+
+//  Write a function to return the maximum number of fruits in both the baskets.
+ 
+
+
+
+import java.util.*;
+
+class MaxFruitCountOf2Types {
+  public static int findLength(char[] arr) {
+    int windowStart = 0, maxLength = 0;
+    Map<Character, Integer> fruitFrequencyMap = new HashMap<>();
+    // try to extend the range [windowStart, windowEnd]
+    for (int windowEnd = 0; windowEnd < arr.length; windowEnd++) {
+      fruitFrequencyMap.put(arr[windowEnd], fruitFrequencyMap.getOrDefault(arr[windowEnd], 0) + 1);
+      // shrink the sliding window, until we are left with '2' fruits in the frequency map
+      while (fruitFrequencyMap.size() > 2) {
+        fruitFrequencyMap.put(arr[windowStart], fruitFrequencyMap.get(arr[windowStart]) - 1);
+        if (fruitFrequencyMap.get(arr[windowStart]) == 0) {
+          fruitFrequencyMap.remove(arr[windowStart]);
+        }
+        windowStart++; // shrink the window
+      }
+      maxLength = Math.max(maxLength, windowEnd - windowStart + 1);
+    }
+
+    return maxLength;
+  }
+
+  public static void main(String[] args) {
+    System.out.println("Maximum number of fruits: " + 
+                          MaxFruitCountOf2Types.findLength(new char[] { 'A', 'B', 'C', 'A', 'C' }));
+    System.out.println("Maximum number of fruits: " + 
+                          MaxFruitCountOf2Types.findLength(new char[] { 'A', 'B', 'C', 'B', 'B', 'C' }));
+  }
+}
