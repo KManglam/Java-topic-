0ed82b9f3edfecf8101d0823d80f5e33d9eb9a27
Kumar Manglam <kmglm04718@gmail.com>
2025-04-25 22:35:22 +0530

    Update fruit.java

--- fruit.java
+++ fruit.java
@@ -2,7 +2,7 @@
 //  With a given array of characters where each character represents a fruit tree, place the maximum number of fruits in each of 2 baskets. The only restriction is that each basket can have only one type of fruit.
 //  You can start with any tree, but you can’t skip a tree once you have started. You will pick one fruit from each tree until you cannot, i.e., you will stop when you have to pick from a third fruit type.
 
-//  Write a function to return the maximum number of fruits in both the baskets.
+//   Write a function to return the maximum number of fruits in both the baskets.
 
 import java.util.*;
 
