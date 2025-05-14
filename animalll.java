4cb165c111a061eb78ac94b7f5b2808ed912cad1
Kumar Manglam <kmglm04718@gmail.com>
2025-05-14 21:57:41 +0530

    Create animalll.java

---
+++ animalll.java
@@ -0,0 +1,38 @@
+
+class Animal {
+    void a() {
+        System.out.println("Parent");
+    }
+}
+
+class Dog extends Animal {
+    void b() {
+        System.out.println("Dog is an Animal.");
+    }
+    Dog() {
+        super.a();
+    }
+    
+    public static void main(String[] args) {
+        Dog obj = new Dog();
+        obj.a();
+        obj.b();
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
