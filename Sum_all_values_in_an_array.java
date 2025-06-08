339445eebb0046b733dc852beab3e856b60e304e
Kumar Manglam <kmglm04718@gmail.com>
2025-06-08 07:42:32 +0530

    Update Sum_all_values_in_an_array.java

--- Sum_all_values_in_an_array.java
+++ Sum_all_values_in_an_array.java
@@ -4,7 +4,6 @@ public class Exercise2 {
     public static void main(String[] args) {      
         int my_array[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
         int sum = 0;
-        // Iterate over each element of the array using an enhanced for loop.
         for (int i : my_array)
             // Add each element to the sum.
             sum += i;
