eb5b133122860cbec0e38884ad85517e3d690f5d
Kumar Manglam <kmglm04718@gmail.com>
2025-06-08 07:41:38 +0530

    Update Sum_all_values_in_an_array.java

--- Sum_all_values_in_an_array.java
+++ Sum_all_values_in_an_array.java
@@ -2,7 +2,6 @@
 public class Exercise2 {
     // The main method where the program execution starts.
     public static void main(String[] args) {      
-        // Declare and initialize an integer array.
         int my_array[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
         // Declare and initialize a variable to store the sum.
         int sum = 0;
